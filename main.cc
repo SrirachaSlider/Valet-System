c752a69a7fec6531d7f6c913de59cf06f0e98f2f
Mohammad Abumaali <mohammadabumaali@macbookpro.lan>
2025-08-02 18:33:04 -0700

    added main.cc

---
+++ main.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Car {
+	int year;
+	string color;
+	string brand;
+	string model;
+	bool present = false; // checks if the car is currently parked or not	
+public:
+	Car(int newYear = 0, string newColor = "None", string newBrand = "None", string newModel = "None") : year(newYear), color(newColor), brand(newBrand), model(newModel) {}
+	int get_year() { return year; }
+	string get_color() { return color; }
+	string get_brand() { return brand; }
+	bool get_present() { return present; }
+};
+
+int main() {
+	unordered_map<int, Car> car;
+	int ticket = 0;
+	int choice = 0;
+	cout << "Enter a number to use the Valet System:\n";
+	while (cin) {
+		// for checking deque for avilaility and stuff, use the order it was put in as the ticket and use the .at() function to help.
+		// ex: if someone enters a white kia at slot 3 and someone does the same for slot 7, to check which one to remove use the .at() and then change present to false
+		cout << "1. Add a car to be parked\n2. Retrieve a car\n3. Quit\n";
+		cin >> choice;
+		if (choice == 1) {
+			//When a car is added to the database, ensure to tell the user the ticket number (bec if there r 2 of the same car, you can use ticket number to retrieve it)
+			
+		}
+		if (choice == 2) {
+			// Use ticket to retrieve car
+			
+		}
+		if (choice == 3) break;
+	}
+}
